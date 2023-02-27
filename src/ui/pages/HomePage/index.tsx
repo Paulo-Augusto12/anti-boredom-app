df586359023153f5f1964d01e056f2a8b7e28917
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-27 20:35:28 -0300

    feat: made title card

--- src/ui/pages/HomePage/index.tsx
+++ src/ui/pages/HomePage/index.tsx
@@ -1,40 +1,39 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import { useHomePage } from "./useHomePage";
+
+import { MdAddTask } from "react-icons/md";
 export function HomePage() {
   const hook = useHomePage();
   return (
     <Container>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "3rem" }}>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
-          <Typography variant="h4" textAlign={"center"}>
-            Procure uma atividade
-          </Typography>
-
-          <Button
-            onClick={() => {
-              hook.handleGetARandomActivity();
-            }}
-          >
-            Procurar
-          </Button>
-        </Box>
-        {hook.activity ? (
+      <Box sx={{ display: "flex", flexDirection: "row", gap: "3rem", p: 2 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            p: 2,
+            backgroundColor: "#BFD7EA",
+            borderRadius: "30px",
+          }}
+        >
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
+              justifyContent: "space-between",
               alignItems: "center",
+              p: 4,
             }}
           >
-            <Typography>{hook.activity.activity}</Typography>
-            <Typography>{hook.activity.type}</Typography>
-            <Typography>{hook.activity.participants}</Typography>
-            <Typography>{hook.activity.link}</Typography>
+            <Typography variant="h4" sx={{ color: "#C81D25", fontWeight: 800 }}>
+              Anti Bored
+            </Typography>
+            <MdAddTask size={64} color={"#FF5A5F"} />
           </Box>
-        ) : (
-          <></>
-        )}
+          <Typography variant="h5" sx={{ color: "#FF5A5F" }}>
+            Veja sugestões de tarefas para te tirar do tédio
+          </Typography>
+        </Box>
       </Box>
     </Container>
   );
