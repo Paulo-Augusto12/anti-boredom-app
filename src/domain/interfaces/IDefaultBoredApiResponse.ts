667f31bb3703175f20bd9cdf3cc9f9d23b6297d0
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-27 18:19:35 -0300

    initial commit

---
+++ src/domain/interfaces/IDefaultBoredApiResponse.ts
@@ -0,0 +1,9 @@
+export interface IDefaultBoredApiResponse {
+  activity: string;
+  accessibility: string;
+  type: string;
+  participants: string;
+  price: string;
+  link: string;
+  key: string;
+}
