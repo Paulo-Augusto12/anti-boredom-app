050c4e24810d066ea600e215ec16ece80106eb1e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-28 10:24:27 -0300

    feat: adicionado link para facilitar visualização em dispositivos mobile

--- src/ui/pages/FilteredAcitivitiesPage/index.tsx
+++ src/ui/pages/FilteredAcitivitiesPage/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography, Link } from "@mui/material";
 import { useFilteredActivitiesPage } from "./useFilteredActivitiesPage";
 import { ActivityCard } from "../../components/ActivityCard";
 
@@ -7,7 +7,14 @@ export function FilteredActivitiesPage() {
   const hook = useFilteredActivitiesPage();
   return (
     <Container>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "3rem", p: 3 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "3rem",
+          p: 3,
+        }}
+      >
         <Box
           sx={{
             display: "flex",
@@ -35,19 +42,22 @@ export function FilteredActivitiesPage() {
               }}
             >
               {hook.apiAvailableTypes.map((option) => (
-                <Button
-                  sx={{
-                    borderRadius: "20px",
-                    backgroundColor: "#0B3954",
-                    "&:hover": { backgroundColor: "#087E8B" },
-                    color: "#FFFF",
-                  }}
-                  onClick={() => {
-                    hook.generateATypeFilteredActivity(option);
-                  }}
-                >
-                  {option}
-                </Button>
+                <Link href="#generatedTask" underline="none">
+                  <Button
+                    sx={{
+                      borderRadius: "20px",
+                      backgroundColor: "#0B3954",
+                      "&:hover": { backgroundColor: "#087E8B" },
+                      color: "#FFFF",
+                      width: "100%",
+                    }}
+                    onClick={() => {
+                      hook.generateATypeFilteredActivity(option);
+                    }}
+                  >
+                    {option}
+                  </Button>
+                </Link>
               ))}
             </Box>
             {hook.activity ? (
@@ -57,10 +67,10 @@ export function FilteredActivitiesPage() {
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
-                    // pt: "2rem",
                   }}
+                  id="generatedTask"
                 >
-                  <Box>
+                  <Box sx={{ scrollBehavior: "smooth", overflowY: "scroll" }}>
                     <Typography
                       variant={"h6"}
                       sx={{ color: "#087E8B", fontWeight: 700 }}
