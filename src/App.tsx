45a7976d4f4347ec20ea0e230cf496bf462c217d
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-01 10:33:38 -0300

    feat: initial development of v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { AppRoutes } from "./ui/routes/Routes";
+import { Container } from "@mui/material";
+import { GlobalStyles } from "@mui/material";
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <GlobalStyles
+        styles={{
+          background:
+            "linear-gradient(221.67deg, #8C23BD 13.33%, #FE55A6 91.24%)",
+        }}
+      />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
--- src/ui/pages/HomePage/index.tsx
+++ src/ui/pages/HomePage/index.tsx
@@ -1,53 +1,27 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Card, Container, Typography } from "@mui/material";
 import { useHomePage } from "./useHomePage";
-
 import { MdAddTask } from "react-icons/md";
 import { OptionLinks } from "../../components/OptionLinks";
 export function HomePage() {
   const hook = useHomePage();
   return (
-    <Container>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "3rem", p: 2 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "2rem",
-            p: 2,
-            backgroundColor: "#BFD7EA",
-            borderRadius: "30px",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              p: 4,
-            }}
-          >
-            <Typography variant="h4" sx={{ color: "#FF5A5F", fontWeight: 800 }}>
-              Bore free
+    <>
+      <Box
+        sx={{
+          background:
+            "linear-gradient(221.67deg, #8C23BD 13.33%, #FE55A6 91.24%);",
+          height: "90vh",
+        }}
+      >
+        <Container>
+          <Card sx={{ backgroundColor: "##613C9D", p: "2rem" }}>
+            <Typography variant="h4" sx={{ fontWeight: 900 }}>
+              Boredom Free
             </Typography>
-            <MdAddTask size={64} color={"#FF5A5F"} />
-          </Box>
-          <Box sx={{ display: "flex", p: 4 }}>
-            <Typography variant="h5" sx={{ color: "#FF5A5F" }}>
-              Get tasks suggestions to get you out of boredom
-            </Typography>
-          </Box>
-        </Box>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "4rem" }}>
-          {hook.options.map((option) => (
-            <OptionLinks
-              buttonTitle={option.button_title}
-              title={option.title}
-              description={option.subtitle}
-              buttonNavigation={() => option.navigation()}
-            />
-          ))}
-        </Box>
+          </Card>
+        </Container>
       </Box>
-    </Container>
+      <Box sx={{ backgroundColor: "#592C6D", height: "20vh" }}></Box>
+    </>
   );
 }
